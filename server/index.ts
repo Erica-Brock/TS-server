e965a3976b5a3af8173460c3c2fbd3bbf0fd1158
Erica-Brock <Eady.Brock@gmail.com>
2017-11-07 17:07:08 -0600

    base

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,36 @@
 import * as E from "express";
 import * as bp from "body-parser";
 import * as fs from "fs"
+import * as path from "path";
+
+
+//make a path that leads to all of my messages which is the collection path
+const app= E();
+const dataPath=path.join(__dirname, 'data.json');
+const api="/api";
+
+
+app
+.disable('x-powered-by')
+.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, UPDATE, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+.use(bp.json())
+.use(bp.urlencoded({extended:true}))
+.use(api, message);
+
+
+//make a path that leads to one single message which is the item path
+//make a get request to the server to get all messages
+//make a get request to the server to get one message
+//make a post request to add a message to the data
+//make a delete request to remove a message from the data
+//make a put request to update an existing message in the data
+//use middleware in some way
+//use modules
+//make a post request
+//make 
+app.listen(3000);
